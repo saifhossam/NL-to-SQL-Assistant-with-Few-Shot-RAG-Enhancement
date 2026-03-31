5fce4d64eab8ebae025c430b245c2af0a3bde95e
Ahmed Essam Hammam <ahmedessamhamam@gmail.com>
2026-03-31 18:14:42 +0200

    Add is_limited parameter to get_natural_response

--- answer_generator.py
+++ answer_generator.py
@@ -1,8 +1,9 @@
 from chains import answer_chain
 
 
-def get_natural_response(question, data):
+def get_natural_response(question, data, is_limited=False):
     return answer_chain.invoke({
         "question": question,
-        "data": data
+        "data": data,
+        "is_limited": str(is_limited)
     }).strip()
